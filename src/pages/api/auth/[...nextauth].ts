8eaf4b88ee467d76e3a21f9638c1f310626f9e90
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-07-30 12:37:49 -0300

    nome de variavel

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,7 +8,7 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),     
     ],
-    secrete: process.env.JWT_SECRET as string,
+    secrete: process.env.NEXTAUTH_SECRET as string,
 }
 
 export default NextAuth(authOptions)
